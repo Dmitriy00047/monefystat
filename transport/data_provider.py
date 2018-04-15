a44b92b9a633cff8b78be538a348308673a4a560
Vladyslav Kotliar <resistendi15@gmail.com>
2018-04-15 12:03:35 +0300

    refactor with "--max-line-length=120", and fix a few issues

--- transport/data_provider.py
+++ transport/data_provider.py
@@ -18,10 +18,8 @@ class DataProvider(object):
                  mode='auto'):
         '''
         :param access_token: dropbox API token.
-        :param download_path: path to folder for providing data
-            (sould include file name).
-        :param working_directory: path to working directory on dropbox
-            where monefy data should be placed.
+        :param download_path: path to folder for providing data (sould include file name).
+        :param working_directory: path to working directory on dropbox where monefy data should be placed.
         :param mode: using for switching workflow, can take only two values:
             'auto' - when dropbox working directory refreshing automatically,
             'manual' - when user refreshing dropbox working directory manually.
@@ -31,10 +29,8 @@ class DataProvider(object):
         :raises: AssertionError, TypeError.
         '''
         assert isinstance(download_path, str), 'path should be str type.'
-        assert working_directory.startswith('/'), \
-            'working directory should starts with \'/\''
-        assert mode == 'auto' or mode == 'manual', \
-            'mode should be \'auto\' or \'manual\''
+        assert working_directory.startswith('/'), 'working directory should starts with \'/\''
+        assert mode == 'auto' or mode == 'manual', 'mode should be \'auto\' or \'manual\''
 
         self.dbx = dropbox.Dropbox(access_token)
         self._download_path = download_path
@@ -64,8 +60,7 @@ class DataProvider(object):
             if value.startswith('/'):
                 self._working_directory = value
             else:
-                raise ValueError(
-                    'Working directory should starts with \'/\'')
+                raise ValueError('Working directory should starts with \'/\'')
 
     @property
     def mode(self):
@@ -80,23 +75,19 @@ class DataProvider(object):
         else:
             self._mode = value
 
-    def __get_newest_monefy_data_name_manual(self, list_of_files: list) -> str:
+    def __get_newest_monefy_data_name_manual(self, list_of_files: list) -> str or None:
         newest_file_index = -1
         newest_file_date = datetime(1, 1, 1)
 
         for i in range(len(list_of_files)):
             if list_of_files[i].startswith('Monefy.Data'):
-                current_file_date = re.search(
-                    r'(\d{1,2}(\-|\.)\d{1,2}(\-|\.)\d{2})',
-                    list_of_files[i]).group()
+                current_file_date = re.search(r'(\d{1,2}(\-|\.)\d{1,2}(\-|\.)\d{2})', list_of_files[i]).group()
                 # parsing american date format
                 if current_file_date.find('-') > 0:
-                    current_file_date = datetime.strptime(
-                        current_file_date, '%m-%d-%y')
+                    current_file_date = datetime.strptime(current_file_date, '%m-%d-%y')
                 # parsing european date format
                 elif current_file_date.find('.') > 0:
-                    current_file_date = datetime.strptime(
-                        current_file_date, '%d.%m.%y')
+                    current_file_date = datetime.strptime(current_file_date, '%d.%m.%y')
                 if current_file_date > newest_file_date:
                     newest_file_date = current_file_date
                     newest_file_index = i
@@ -106,7 +97,7 @@ class DataProvider(object):
         else:
             return None
 
-    def __get_newest_monefy_data_name_auto(self) -> str:
+    def __get_newest_monefy_data_name_auto(self) -> str or None:
         if self._working_directory != '/':
             directory = self._working_directory
         else:
@@ -123,8 +114,7 @@ class DataProvider(object):
 
     def get_files_list(self) -> list:
         '''
-        This method returns list of file and folder names which
-        contains in app folder.
+        This method returns list of file and folder names which contains in app folder.
 
         :raises: dropbox.exceptions.ApiError.
         '''
@@ -141,7 +131,7 @@ class DataProvider(object):
 
     def get_file_by_name(self, file_name: str) -> None:
         '''
-        This method download file with 'file_name' to 'download_path'.
+        This method download file with 'file_name' to 'self.download_path'.
 
         :param file_name: name of file to download.
         :raises: dropbox.exceptions.ApiError.
@@ -151,17 +141,16 @@ class DataProvider(object):
 
     def get_newest_monefy_data(self) -> None:
         '''
-        This method download newest monefy.data file to 'download_path'.
+        This method download newest monefy.data file to 'self.download_path'.
 
         :raises: dropbox.exceptions.ApiError.
         '''
+        newest_file_name = None
+
         if self._mode == 'manual':
-            newest_file_name = \
-                self.__get_newest_monefy_data_name_manual(
-                    self.get_files_list())
+            newest_file_name = self.__get_newest_monefy_data_name_manual(self.get_files_list())
         elif self._mode == 'auto':
-            newest_file_name = \
-                self.__get_newest_monefy_data_name_auto()
+            newest_file_name = self.__get_newest_monefy_data_name_auto()
 
         if newest_file_name:
             if self._working_directory == '/':
