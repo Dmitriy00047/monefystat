9ba960a6dd668d775697adc3081f1ef084c144e7
egoortus <dima.kibetc@gmail.com>
2018-04-24 14:42:30 +0300

    All fixes to run

--- processor/validator_data.py
+++ processor/validator_data.py
@@ -16,7 +16,7 @@ def validate_data(file_csv) -> list:
     :raises: ValidationError
     """
     types = [date, str, str, float, str, float, str, str]
-    result = list()
+    result = []
     # validate data
     with open(file_csv, 'r', encoding='utf8') as inf:
         title_file = inf.readline().strip()
@@ -25,20 +25,23 @@ def validate_data(file_csv) -> list:
 
         # convert all the fields in a row by type
         for line in inf:
-            current_string = line.strip().split(',')
-            description = current_string[7]
+            if line != '\n':
+                current_string = line.strip().split(',')
+                description = current_string[7] if len(current_string) == 8 else ''
 
-            # the following two lines to that the fields don't have spaces except the description field
-            current_string = list(map(lambda x: x.replace(" ", ""), current_string[:7]))
-            current_string = list(map(lambda x: x.replace("\xa0", ""), current_string[:7]))
-            current_string.append(description)
+                # the following two lines to that the fields don't have spaces except the description field
+                current_string = list(map(lambda x: x.replace(" ", ""), current_string[:7]))
+                current_string = list(map(lambda x: x.replace("\xa0", ""), current_string[:7]))
+                current_string.append(description)
 
-            # the following two lines convert str with date into datetime.date
-            current_string[0] = current_string[0].split('/')[::-1]
-            current_string[0] = date(int(current_string[0][0]), int(current_string[0][1]), int(current_string[0][2]))
+                # the following two lines convert str with date into datetime.date
+                current_string[0] = current_string[0].split('/')[::-1]
+                if current_string[0] != ['']:
+                    current_string[0] = date(int(current_string[0][0]), int(current_string[0][1]), int(current_string[0][2]))
 
-            # convert the rest of the fields
-            for i in range(1, len(types)):
-                current_string[i] = types[i](current_string[i])
-            result.append(current_string)
+                # convert the rest of the fields
+                for i in range(1, len(types)):
+                    if len(current_string) == 8:
+                        current_string[i] = types[i](current_string[i])
+                result.append(current_string)
     return result
