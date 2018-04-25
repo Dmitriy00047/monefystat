6d7e4008a9035c56fc9a701b4149c7bf63b5feb1
Vladyslav Kotliar <resistendi15@gmail.com>
2018-04-25 23:39:55 +0300

    select, insert, update, delete methods for limiter (TODO: refactor)

--- database/helpers.py
+++ database/helpers.py
@@ -1,11 +1,18 @@
 import psycopg2
+from datetime import datetime
 from aiopg.sa import create_engine
 from config import db
 from sqlalchemy.schema import CreateTable, DropTable
+from sqlalchemy import select
 from database.models import Transaction
+from database.models import Category
+from sqlalchemy import create_engine as cren
 
-dsn_def = 'user={user} password={password} host={host} port={port}'.format(**db)
-dsn = 'user={user} dbname={dbname} host={host} password={password}'.format(**db)
+# dsn_def = 'user={user} password={password} host={host} port={port}'.format(**db)
+# dsn = 'user={user} dbname={dbname} host={host} password={password}'.format(**db)
+
+dsn_def = 'postgresql://user:password@localhost:5432'
+dsn = 'postgresql://user:password@localhost:5432/monefystat'
 
 
 async def _create_default_engine():
@@ -39,7 +46,7 @@ async def drop_db():
 
 async def _prepare_tables():
     """Asynchronous function for creating tables in database."""
-    tables = [Transaction.__table__]
+    tables = [Category.__table__, Transaction.__table__]
     engine = await _create_engine()
     async with engine:
         async with engine.acquire() as connection:
@@ -88,3 +95,63 @@ def _convert_resultproxy_to_dictionary(result_proxy):
     for row in result_proxy:
         dict_result.append(dict(row))
     return dict_result
+
+
+async def get_limit(category_name=None) -> list:
+    """
+    Asynchronous function for getting data from category table.
+
+    :param str category_name: name of selecting category.
+    :return list: list of dictionaries like this:
+        [
+            {
+                'id': int,
+                'timestamp': datetime,
+                'title': category_name,
+                'limit': float,
+                'start_date': datetime,
+                'period': int,
+                'is_repeated': bool
+            }
+        ]
+    """
+    engine = cren(dsn)
+    conn = engine.connect()
+    if category_name:
+        s = select([Category]).where(Category.title == category_name)
+    else:
+        s = select([Category])
+
+    result = conn.execute(s)
+    return _convert_resultproxy_to_dictionary(result)
+
+
+async def add_limit(title: str, limit: float, start_date: datetime, period: int, is_repeated: bool) -> None:
+    args = {
+        'title': title,
+        'limit': limit,
+        'start_date': start_date,
+        'period': period,
+        'is_repeated': is_repeated
+    }
+    category = Category.__table__
+    ins = category.insert().values(args)
+    engine = cren(dsn)
+    conn = engine.connect()
+    conn.execute(ins)
+
+
+async def update_limit(category_name: str, **kwargs):
+    category = Category.__table__
+    upd = category.update().where(Category.title == category_name).values(kwargs)
+    engine = cren(dsn)
+    conn = engine.connect()
+    conn.execute(upd)
+
+
+async def delete_limit(category_name: str):
+    category = Category.__table__
+    delete = category.delete().where(Category.title == category_name)
+    engine = cren(dsn)
+    conn = engine.connect()
+    conn.execute(delete)
