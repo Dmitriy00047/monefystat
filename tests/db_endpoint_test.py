ab0e6ee9a98dad19fecef9c44b2b3205e511fa4c
Antyfreezze <neggrr@gmail.com>
2018-04-29 00:45:45 +0300

    Refact quqry function

--- tests/db_endpoint_test.py
+++ tests/db_endpoint_test.py
@@ -24,7 +24,7 @@ class Data_db_test(unittest.TestCase):
 
 class Defined_period_test(unittest.TestCase):
     def test_get_data_for_def_period_returns_200(self):
-        params = {"category_name": "еда", "period": "7"}
+        params = {"category": "еда", "period": "7"}
         request, response = app.test_client.get("/data_def_period", params=params)
         assert response.status == 200
 
@@ -33,14 +33,14 @@ class Defined_period_test(unittest.TestCase):
         assert response.status == 405
 
     def test_get_data_for_def_period_query_in_get(self):
-        params = {"category_name": "еда", "period": "7"}
+        params = {"category": "еда", "period": "7"}
         request, response = app.test_client.get("/data_def_period", params=params)
-        assert request.args.get("category_name") == "еда" and request.args.get("period") == "7"
+        assert request.args.get("category") == "еда" and request.args.get("period") == "7"
 
 
 class Custom_period_test(unittest.TestCase):
     def test_get_data_for_custom_period_returns_200(self):
-        params = {"category_name": "еда", "start_date": "2018-03-25", "end_date": "2018-04-25"}
+        params = {"category": "еда", "start_date": "2018-03-25", "end_date": "2018-04-25"}
         request, response = app.test_client.get("/data_custom_period", params=params)
         assert response.status == 200
 
@@ -49,9 +49,9 @@ class Custom_period_test(unittest.TestCase):
         assert response.status == 405
 
     def test_get_data_for_custom_period_query_in_get(self):
-        params = {"category_name": "еда", "start_date": "2018-03-25", "end_date": "2018-04-25"}
+        params = {"category": "еда", "start_date": "2018-03-25", "end_date": "2018-04-25"}
         request, response = app.test_client.get("/data_custom_period", params=params)
-        assert request.args.get("category_name") == "еда" and \
+        assert request.args.get("category") == "еда" and \
             request.args.get("start_date") == "2018-03-25" and \
             request.args.get("end_date") == "2018-04-25"
 
