e476eea48e848d9b0edb2b8476bc601f100f30fa
vk <kotliarvladyslav@gmail.com>
2018-04-30 23:36:44 +0300

    add dynamic _prepare_tables()

--- database/helpers.py
+++ database/helpers.py
@@ -1,8 +1,16 @@
-import psycopg2
+import sys
+import inspect
+from typing import List, Dict
+from collections import namedtuple
 from datetime import datetime, timedelta
+
+import psycopg2
 from aiopg.sa import create_engine
-from sqlalchemy.schema import CreateTable, DropTable
 from sqlalchemy import select, and_
+from sqlalchemy.schema import CreateTable, DropTable
+from sqlalchemy.dialects.postgresql import insert
+
+from database import models
 from database.models import Transaction, Category
 from config import db
 
@@ -39,9 +47,31 @@ async def drop_db() -> None:
             await connection.execute('drop database {}'.format(db['dbname']))
 
 
+async def _get_model_classes() -> list:
+    ''' Asynchronous function for getting model classes. '''
+    Clsmember = namedtuple('Clsmember', ['cls_name', 'cls_def'])
+    clsmembers = [Clsmember(member[0], member[1]) for member in inspect.getmembers(sys.modules[models.__name__],
+                                                                                   inspect.isclass)]
+
+    accumulator = []
+    for el in clsmembers:
+        if el.cls_name != models.Base.__name__ and issubclass(el.cls_def, models.Base):
+            accumulator.append(el)
+    return accumulator
+
+
+async def _get_tables() -> list:
+    ''' Asynchronous function for getting tables. '''
+    model_classes = await _get_model_classes()
+    result = []
+    for model in model_classes:
+        result.append(model.cls_def.__table__)
+    return result
+
+
 async def _prepare_tables() -> None:
     '''Asynchronous function for creating tables in database.'''
-    tables = [Category.__table__, Transaction.__table__]
+    tables = await _get_tables()
     engine = await _create_engine()
     async with engine:
         async with engine.acquire() as connection:
@@ -140,3 +170,72 @@ def _date_validator(period=None, start_date=None, end_date=None) -> tuple:
         except ValueError:
             return 'Invalid dates'
     return start_date, end_date
+
+
+async def get_limit(category_name=None) -> List[Dict]:
+    '''
+    Asynchronous function for getting data from category table.
+
+    :param str category_name: name of selecting category.
+    :return list: list of dictionaries like this:
+        [
+            {
+                'id': int,
+                'timestamp': datetime,
+                'title': category_name,
+                'limit': float,
+                'start_date': datetime,
+                'period': int,
+                'is_repeated': bool
+            }
+        ]
+    '''
+    engine = await _create_engine()
+    async with engine:
+        async with engine.acquire() as connection:
+            if category_name:
+                s = select([Category]).where(Category.title == category_name)
+            else:
+                s = select([Category])
+
+            result = await connection.execute(s)
+            return _convert_resultproxy_to_dictionary(result)
+
+
+async def upsert_limit(category_name, **kwargs) -> None:
+    '''
+    Asynchronous function for inserting and updating data.
+
+    Warning: **kwargs must accept only the specified parameters.
+
+    :param str category_name: name of selecting category.
+    :param float limit: limit of categody.
+    :param datetime start_date: start day of limit.
+    :param int period: number of days for limit.
+    :param bool is_repeated: checking limit should be repeated for the same period.
+    :rtype: None
+    '''
+    engine = await _create_engine()
+    async with engine:
+        async with engine.acquire() as connection:
+            ins = insert(Category).values(dict(title=category_name, **kwargs))
+            do_update_category = ins.on_conflict_do_update(index_elements=['title'], set_=kwargs)
+            await connection.execute(do_update_category)
+
+
+async def delete_limit(category_name: str) -> None:
+    '''
+    Asynchronous function function for clearing limit.
+
+    :param str category_name: name of category to clean.
+    :rtype: None.
+    '''
+    category = Category.__table__
+    engine = await _create_engine()
+    async with engine:
+        async with engine.acquire() as connection:
+            delete = category.update().where(Category.title == category_name).values(limit=None,
+                                                                                     start_date=None,
+                                                                                     period=None,
+                                                                                     is_repeated=None)
+            await connection.execute(delete)
