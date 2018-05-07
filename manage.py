159d876f384eb94e86aeefec7e64c35d21798402
Dmitriy00047 <dima.kibetc@gmail.com>
2018-05-07 17:14:10 +0300

    added telegram bot launch script

--- manage.py
+++ manage.py
@@ -1,6 +1,10 @@
+from multiprocessing import Process
+from telegram_bot.bot_handlers import bot
 from monefystat_api import app
 from config import web
 
-
 if __name__ == '__main__':
+    p = Process(target=bot.polling, kwargs={'none_stop': True})
+    p.start()
+    p.join()
     app.app.run(host=web['host'], port=web['port'])
