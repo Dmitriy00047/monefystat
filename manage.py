149e10f5c7a0e738cfccb388d4383f17ed090fc5
Dmitriy00047 <dima.kibetc@gmail.com>
2018-05-08 17:09:58 +0300

    changed process flow

--- manage.py
+++ manage.py
@@ -4,7 +4,11 @@ from monefystat_api import app
 from config import web
 
 if __name__ == '__main__':
-    p = Process(target=bot.polling, kwargs={'none_stop': True})
-    p.start()
-    p.join()
-    app.app.run(host=web['host'], port=web['port'])
+    p1 = Process(target=bot.polling, kwargs={'none_stop': True})
+    p1.start()
+
+    p2 = Process(target=app.app.run, kwargs={'host': web['host'], 'port': web['port']})
+    p2.start()
+
+    p1.join()
+    p2.join()
