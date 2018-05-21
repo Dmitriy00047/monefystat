cf34ae25f2c174fbfd9d9743b2d17e4f6c0eac3a
Antyfreezze <NoPiece@Nopiece.localdomain>
2018-05-21 15:16:24 +0200

    add create setup/teardown for limit_endpoint_test.py

--- tests/db_endpoint_test.py
+++ tests/db_endpoint_test.py
@@ -1,5 +1,4 @@
 import unittest
-import json
 from monefystat_api.app import app
 
 
@@ -69,63 +68,63 @@ class DCustomPeriodTest(unittest.TestCase):
             request.args.get('end_date') == '25-04-2018'
 
 
-class EUpsertLimitTest(unittest.TestCase):
-    @unittest.skip('travis fail')
-    def test_post_limit_valid(self):
-        body = {
-            'category_name': 'taxi',
-            'limit': '34',
-            'start_date': '01-01-2012',
-            'period': '34',
-            'is_repeated': 'false'
-        }
-        request, response = app.test_client.post('/limit', data=json.dumps(body))
-        assert response.status == 200
+# class EUpsertLimitTest(unittest.TestCase):
+#     @unittest.skip('travis fail')
+#     def test_post_limit_valid(self):
+#         body = {
+#             'category_name': 'taxi',
+#             'limit': '34',
+#             'start_date': '01-01-2012',
+#             'period': '34',
+#             'is_repeated': 'false'
+#         }
+#         request, response = app.test_client.post('/limit', data=json.dumps(body))
+#         assert response.status == 200
 
-    @unittest.skip('travis fail')
-    def test_put_limit_invalid(self):
-        body = {
-            'category_name': 'taxi',
-            'limit': '-34',
-            'start_date': '01-01-2012',
-            'period': '34',
-            'is_repeated': 'false'
-        }
-        request, response = app.test_client.put('/limit', data=json.dumps(body))
-        assert response.status == 400
-
-
-class FDeleteLimitTest(unittest.TestCase):
-    @unittest.skip('travis fail')
-    def test_delete_limit_exists(self):
-        data = {'category_name': 'taxi'}
-        request, response = app.test_client.delete('/limit', data=json.dumps(data))
-        assert response.status == 200
-
-    @unittest.skip('travis fail')
-    def test_delete_limit_not_exists(self):
-        data = {'category_name': 'car'}
-        request, response = app.test_client.delete('/limit', data=json.dumps(data))
-        assert response.status == 404
+#     @unittest.skip('travis fail')
+#     def test_put_limit_invalid(self):
+#         body = {
+#             'category_name': 'taxi',
+#             'limit': '-34',
+#             'start_date': '01-01-2012',
+#             'period': '34',
+#             'is_repeated': 'false'
+#         }
+#         request, response = app.test_client.put('/limit', data=json.dumps(body))
+#         assert response.status == 400
 
 
-class GGetLimitTest(unittest.TestCase):
-    @unittest.skip('travis fail')
-    def test_get_concrete_limit(self):
-        params = {'category_name': 'taxi'}
-        request, response = app.test_client.get('/limit', params=params)
-        assert response.status == 200
+# class FDeleteLimitTest(unittest.TestCase):
+#     @unittest.skip('travis fail')
+#     def test_delete_limit_exists(self):
+#         data = {'category_name': 'taxi'}
+#         request, response = app.test_client.delete('/limit', data=json.dumps(data))
+#         assert response.status == 200
 
-    @unittest.skip('travis fail')
-    def test_get_all_limits(self):
-        request, response = app.test_client.get('/limit')
-        assert response.status == 200
+#     @unittest.skip('travis fail')
+#     def test_delete_limit_not_exists(self):
+#         data = {'category_name': 'car'}
+#         request, response = app.test_client.delete('/limit', data=json.dumps(data))
+#         assert response.status == 404
 
-    @unittest.skip('travis fail')
-    def test_get_not_existig_limits(self):
-        params = {'category_name': 'tax'}
-        request, response = app.test_client.get('/limit', params=params)
-        assert response.status == 404
+
+# class GGetLimitTest(unittest.TestCase):
+#     @unittest.skip('travis fail')
+#     def test_get_concrete_limit(self):
+#         params = {'category_name': 'taxi'}
+#         request, response = app.test_client.get('/limit', params=params)
+#         assert response.status == 200
+
+#     @unittest.skip('travis fail')
+#     def test_get_all_limits(self):
+#         request, response = app.test_client.get('/limit')
+#         assert response.status == 200
+
+#     @unittest.skip('travis fail')
+#     def test_get_not_existig_limits(self):
+#         params = {'category_name': 'tax'}
+#         request, response = app.test_client.get('/limit', params=params)
+#         assert response.status == 404
 
 
 class JDropTest(unittest.TestCase):
